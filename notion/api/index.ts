a8ab0aaa850e15a2def0b654e6c5097e9da2d21b
caojunjie <1301239018@qq.com>
2023-05-04 20:08:44 +0800

    feat(vercel-deploy) vercel的部署

--- notion/api/index.ts
+++ notion/api/index.ts
@@ -1,24 +1,5 @@
-import { handleMergeRequest } from './service';
-import express from 'express';
-import bodyParser from 'body-parser';
+const app = require('express')();
 const { v4 } = require('uuid');
-
-var app = express();
-
-// 使用 body-parser 中间件解析请求体
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world');
-});
-
-// 每周都得变更一次id?
-app.post('/gitlab-merge-request', async function (req, res) {
-  await handleMergeRequest(req.body);
-  res.send(JSON.stringify({ msg: 'success' }));
-});
 
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
@@ -39,4 +20,4 @@ app.get('/api/item/:slug', (req, res) => {
   res.end(`Item: ${slug}`);
 });
 
-export default app;
+module.exports = app;
---
+++ notion/app.ts
@@ -0,0 +1,7 @@
+import app from '.';
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
